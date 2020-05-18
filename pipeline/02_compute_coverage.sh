46a8de1e063e95773b606de697aac6de0b62403e
Jason Stajich <jasonstajich.phd@gmail.com>
2020-05-18 16:25:26 -0700

    use parallel

--- pipeline/02_compute_coverage.sh
+++ pipeline/02_compute_coverage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-#SBATCH -p short --mem 64gb -N 1 -n 4 --out logs/coverage.log
+#SBATCH -p short --mem 64gb -N 1 -n 24 --out logs/coverage.log
 
 module load bedtools
 module unload perl
@@ -14,10 +14,11 @@ CPUS=$CPU
 
 BAM=bam
 GENOME=genome/Oryza_sativa.IRGSP-1.0.30.dna.toplevel.fa
-COV=coverage
+COV=coverage/windows
 mkdir -p $COV
 # could do this with parallel with 2 arguments but that's prob even more confusing...
 for WINDOW in 5000 10000 1000
 do
-    parallel -j $CPUS mosdepth -f $GENOME -x -n --by $WINDOW $COV/{/.}_w${WINDOW} {} ::: $(ls $BAM/*/*.bam)
+    parallel --rpl '{///} $Global::use{"File::Basename"} ||= eval "use File::Basename; 1;"; $_ = basename(dirname($_));' \
+		 -j $CPUS mosdepth -f $GENOME -x -n --by $WINDOW $COV/{///}_{/.}_w${WINDOW} {} ::: $(ls $BAM/*/*.bam)
 done
