d48885e282359b3e059c1ec3273268b8c26367e9
Ying Sun <ysun@ucr.edu>
2020-05-19 15:59:13 -0700

    added code for A123 and A119

--- pipeline/03_empty_sites.sh
+++ pipeline/03_empty_sites.sh
@@ -20,19 +20,36 @@ fi
 # genome lengths file
 cut -f 1,2 $GENOME.fai > $GENOME.genome
 
-COV=coverage/pre_mPing
-TMP=tmp
+COV=coverage/processed_empty
+TMP=03_processed_empty
 mkdir -p $COV $TMP
+
+# For HEG4 and EG4
 # Get HEG4 empty sites (mPing in EG4 but not in HEG4)
 bedtools intersect -a $MPING/EG4.gff -b $MPING/HEG4.gff -v | cut -f 1,4,5,9 | perl -p -e 's/^Chr//; s/ID=//' > $TMP/HEG4_empty_sites.bed
 # an vice versa
 bedtools intersect -a $MPING/HEG4.gff -b $MPING/EG4.gff -v | cut -f 1,4,5,9 | perl -p -e 's/^Chr//; s/ID=//' > $TMP/EG4_empty_sites.bed
 
+
+# For A123 and A119
+# Get A123 empty sites (mPing in A119 but not in A123)
+bedtools intersect -a $MPING/A119.gff -b $MPING/A123.gff -v | cut -f 1,4,5,9 | perl -p -e 's/^Chr//; s/ID=//' > $TMP/A123_empty_sites.bed
+# an vice versa
+bedtools intersect -a $MPING/A123.gff -b $MPING/A119.gff -v | cut -f 1,4,5,9 | perl -p -e 's/^Chr//; s/ID=//' > $TMP/A119_empty_sites.bed
+
+# use -b to add to both sides
 bedtools slop -i $TMP/EG4_empty_sites.bed -g $GENOME.genome -b 250  > $TMP/EG4_empty.500_window.bed
 bedtools slop -i $TMP/EG4_empty_sites.bed -g $GENOME.genome -b 500  > $TMP/EG4_empty.1000_window.bed
 
 bedtools slop -i $TMP/HEG4_empty_sites.bed -g $GENOME.genome -b 250 > $TMP/HEG4_empty.500_window.bed
-bedtools slop -i $TMP/HEG4_empty_sites.bed -g $GENOME.genome -b 250 > $TMP/HEG4_empty.1000_window.bed
+bedtools slop -i $TMP/HEG4_empty_sites.bed -g $GENOME.genome -b 500 > $TMP/HEG4_empty.1000_window.bed
+
+bedtools slop -i $TMP/A123_empty_sites.bed -g $GENOME.genome -b 250  > $TMP/A123_empty.500_window.bed
+bedtools slop -i $TMP/A123_empty_sites.bed -g $GENOME.genome -b 500  > $TMP/A123_empty.1000_window.bed
+
+bedtools slop -i $TMP/A119_empty_sites.bed -g $GENOME.genome -b 250 > $TMP/A119_empty.500_window.bed
+bedtools slop -i $TMP/A119_empty_sites.bed -g $GENOME.genome -b 500 > $TMP/A119_empty.1000_window.bed
+
 
 for BED in $(ls $TMP/*_window.bed)
 do
