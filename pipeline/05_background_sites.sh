a5535fbf357dfe072e942016f83c5cad77bd06d0
Ying Sun <ysun@ucr.edu>
2020-05-22 16:19:56 -0700

    edit file order

--- pipeline/05_background_sites.sh
+++ pipeline/05_background_sites.sh
@@ -38,17 +38,30 @@ bedtools slop -i $TMP/Simulate0001_background_sites.bed -g $GENOME.genome -b 500
 bedtools slop -i $TMP/Simulate0002_background_sites.bed -g $GENOME.genome -b 250  > $TMP/Simulate0002_background_sites.500_window.bed
 bedtools slop -i $TMP/Simulate0002_background_sites.bed -g $GENOME.genome -b 500  > $TMP/Simulate0002_background_sites.1000_window.bed
 
-BAM=bam
-GENOME=genome/Oryza_sativa.IRGSP-1.0.30.dna.toplevel.fa
-COV=coverage/processed_background
-mkdir -p $COV
-# could do this with parallel with 2 arguments but that's prob even more confusing...
-for WINDOW in 10000 5000 1000 500
+for BED in $(ls $TMP/*_window.bed)
 do
-    parallel --rpl '{///} $Global::use{"File::Basename"} ||= eval "use File::Basename; 1;"; $_ = basename(dirname($_));' \
-		 -j $CPUS mosdepth -f $GENOME -x -n --by $WINDOW $COV/{///}_{/.}_w${WINDOW} {} ::: $(ls $BAM/*/*.bam)
+	NEWBASE=$(basename $BED .bed)
+	for BAMFILE  in $(ls $BAM/*/*.bam)
+	do
+		EPI=ATAC
+		mosdepth -t $CPUS -f $GENOME -n -x --by $BED $COV/${EPI}.${NEWBASE} $BAMFILE
+	done
 done
 
+for BED in $(ls $TMP/*_window.bed)
+do
+	NEWBASE=$(basename $BED .bed)
+	STRAIN=$(echo $NEWBASE | perl -p -e 's/_empty\.\S+//')
+	# could replace this with parallel code
+	parallel --rpl '{///} $Global::use{"File::Basename"} ||= eval "use File::Basename; 1;"; $_ = basename(dirname($_));' \
+		 -j $CPUS mosdepth -f $GENOME -x -n --by $BED $COV/{///}_{/.}${NEWBASE} {} ::: $(ls $BAM/*/*.bam)
+	#for BAMFILE  in $(ls $BAM/*/$STRAIN.*.bam)
+	#do
+	#	EPI=$(basename `dirname $BAMFILE`)
+	#	SAMPLE=$(basename $BAMFILE .bam)
+	#	mosdepth -t $CPUS -f $GENOME -n -x --by $BED $COV/${EPI}.${SAMPLE}.${NEWBASE} $BAMFILE
+	#done
+done
 
 mkdir coverage/processed_background/tmp
 mv coverage/processed_background/*bed.gz.csi coverage/processed_background/tmp
@@ -65,4 +78,3 @@ mv coverage/processed_background/ATAC* coverage/processed_background/ATAC
 mv coverage/processed_background/H3K27me3* coverage/processed_background/H3K27me3
 mv coverage/processed_background/H3K36me3* coverage/processed_background/H3K36me3
 mv coverage/processed_background/H3K56ac* coverage/processed_background/H3K56ac
-
