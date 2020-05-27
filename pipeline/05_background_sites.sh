19b71e680070dd0ae0f5f2cdbf5898ee413b9f79
Jason Stajich <jasonstajich.phd@gmail.com>
2020-05-26 20:55:09 -0700

    simplify code - write to the folder, and use all the EPI marks

--- pipeline/05_background_sites.sh
+++ pipeline/05_background_sites.sh
@@ -24,76 +24,31 @@ fi
 cut -f 1,2 $GENOME.fai > $GENOME.genome
 
 COV=coverage/processed_background
-TMP=05_processed_background
-mkdir -p $COV $TMP
+TMP=tmp/05_processed_background # I like to keep a clean folder so I would use tmp/05_processed_background
+ # then you can designate in your .gitignore file tmp/*
+ # and also have a cleanup step that rm -rf tmp
 
+mkdir -p $COV $TMP
 # For HEG4 and EG4
 # Get HEG4 empty sites (mPing in EG4 but not in HEG4)
-cat $MPING/Simulate0001_100.gff | cut -f 1,4,5,9 | perl -p -e 's/^Chr//; s/ID=//' > $TMP/Simulate0001_100_background_sites.bed
-cat $MPING/Simulate0001_165.gff | cut -f 1,4,5,9 | perl -p -e 's/^Chr//; s/ID=//'  > $TMP/Simulate0001_165_background_sites.bed
-cat $MPING/Simulate0001_208.gff | cut -f 1,4,5,9 | perl -p -e 's/^Chr//; s/ID=//'  > $TMP/Simulate0001_208_background_sites.bed
-cat $MPING/Simulate0001_310.gff | cut -f 1,4,5,9 | perl -p -e 's/^Chr//; s/ID=//'  > $TMP/Simulate0001_310_background_sites.bed
-cat $MPING/Simulate0001_99.gff | cut -f 1,4,5,9 | perl -p -e 's/^Chr//; s/ID=//'  > $TMP/Simulate0001_99_background_sites.bed
-cat $MPING/Simulate0002.gff | cut -f 1,4,5,9 | perl -p -e 's/^Chr//; s/ID=//'  > $TMP/Simulate0002_background_sites.bed
+for infile in $(ls $MPING/*.gff)
+do
+    base=$(basename $infile .gff)
+    background=$TMP/${base}_background_sites
+    cut -f1,4,5,9 $infile | perl -p -e 's/^Chr//; s/ID=//;' > $background.bed
 
-
-# use -b to add to both sides
-bedtools slop -i $TMP/Simulate0001_100_background_sites.bed -g $GENOME.genome -b 250  > $TMP/Simulate0001_100_background_sites.500_window.bed
-bedtools slop -i $TMP/Simulate0001_100_background_sites.bed -g $GENOME.genome -b 500  > $TMP/Simulate0001_100_background_sites.1000_window.bed
-
-bedtools slop -i $TMP/Simulate0001_165_background_sites.bed -g $GENOME.genome -b 250  > $TMP/Simulate0001_165_background_sites.500_window.bed
-bedtools slop -i $TMP/Simulate0001_165_background_sites.bed -g $GENOME.genome -b 500  > $TMP/Simulate0001_165_background_sites.1000_window.bed
-
-bedtools slop -i $TMP/Simulate0001_208_background_sites.bed -g $GENOME.genome -b 250  > $TMP/Simulate0001_208_background_sites.500_window.bed
-bedtools slop -i $TMP/Simulate0001_208_background_sites.bed -g $GENOME.genome -b 500  > $TMP/Simulate0001_208_background_sites.1000_window.bed
-
-bedtools slop -i $TMP/Simulate0001_310_background_sites.bed -g $GENOME.genome -b 250  > $TMP/Simulate0001_310_background_sites.500_window.bed
-bedtools slop -i $TMP/Simulate0001_310_background_sites.bed -g $GENOME.genome -b 500  > $TMP/Simulate0001_310_background_sites.1000_window.bed
-
-bedtools slop -i $TMP/Simulate0001_99_background_sites.bed -g $GENOME.genome -b 250  > $TMP/Simulate0001_99_background_sites.500_window.bed
-bedtools slop -i $TMP/Simulate0001_99_background_sites.bed -g $GENOME.genome -b 500  > $TMP/Simulate0001_99_background_sites.1000_window.bed
-
-bedtools slop -i $TMP/Simulate0002_background_sites.bed -g $GENOME.genome -b 250  > $TMP/Simulate0002_background_sites.500_window.bed
-bedtools slop -i $TMP/Simulate0002_background_sites.bed -g $GENOME.genome -b 500  > $TMP/Simulate0002_background_sites.1000_window.bed
-
+    # use -b to add to both sides
+    bedtools slop -i $background.bed -g $GENOME.genome -b 250  > $background.1000_window.bed
+    bedtools slop -i $background.bed -g $GENOME.genome -b 500  > $background.500_window.bed
+done
 
 for BED in $(ls $TMP/*_window.bed)
 do
 	NEWBASE=$(basename $BED .bed)
-	for BAMFILE  in $(ls $BAM/*/*.bam)
+	for BAMFILE in $(ls $BAM/*/*.bam)
 	do
-		EPI=ATAC
-		mosdepth -t $CPUS -f $GENOME -n -x --by $BED $COV/${EPI}.${NEWBASE} $BAMFILE
+		EPI=$(basename `dirname $BAMFILE`)
+		mkdir -p $COV/$EPI
+		mosdepth -t $CPUS -f $GENOME -n -x --by $BED $COV/${EPI}/${EPI}.${NEWBASE} $BAMFILE
 	done
 done
-
-for BED in $(ls $TMP/*_window.bed)
-do
-	NEWBASE=$(basename $BED .bed)
-	STRAIN=$(echo $NEWBASE | perl -p -e 's/_empty\.\S+//')
-	# could replace this with parallel code
-	parallel --rpl '{///} $Global::use{"File::Basename"} ||= eval "use File::Basename; 1;"; $_ = basename(dirname($_));' \
-		 -j $CPUS mosdepth -f $GENOME -x -n --by $BED $COV/{///}_{/.}${NEWBASE} {} ::: $(ls $BAM/*/*.bam)
-	#for BAMFILE  in $(ls $BAM/*/$STRAIN.*.bam)
-	#do
-	#	EPI=$(basename `dirname $BAMFILE`)
-	#	SAMPLE=$(basename $BAMFILE .bam)
-	#	mosdepth -t $CPUS -f $GENOME -n -x --by $BED $COV/${EPI}.${SAMPLE}.${NEWBASE} $BAMFILE
-	#done
-done
-
-mkdir coverage/processed_background/tmp
-mv coverage/processed_background/*bed.gz.csi coverage/processed_background/tmp
-mv coverage/processed_background/*global.dist.txt coverage/processed_background/tmp
-mv coverage/processed_background/*region.dist.txt coverage/processed_background/tmp
-mv coverage/processed_background/*.summary.txt coverage/processed_background/tmp
-
-mkdir coverage/processed_background/ATAC
-mkdir coverage/processed_background/H3K27me3
-mkdir coverage/processed_background/H3K36me3
-mkdir coverage/processed_background/H3K56ac
-
-mv coverage/processed_background/ATAC* coverage/processed_background/ATAC
-mv coverage/processed_background/H3K27me3* coverage/processed_background/H3K27me3
-mv coverage/processed_background/H3K36me3* coverage/processed_background/H3K36me3
-mv coverage/processed_background/H3K56ac* coverage/processed_background/H3K56ac
