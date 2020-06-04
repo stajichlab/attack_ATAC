baf77454343d024e05e9686c55c21e6512f16ea8
Jason Stajich <jasonstajich.phd@gmail.com>
2020-06-04 15:21:41 -0700

    run with array jobs to separate simulations

--- Simulation_of_mPing_insertions/mPing_sim_9merTSDV2_Random.sh
+++ Simulation_of_mPing_insertions/mPing_sim_9merTSDV2_Random.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem=100G
-#SBATCH --time=4:00:00
+#SBATCH --mem=96G
+#SBATCH --time=8:00:00
 #SBATCH --output=mPing_sim_9merTSDV2_Random.sh.%A_%a.stdout
 #SBATCH -p intel
-#SBATCH --chdir=./
 
+module unload miniconda3
+module load miniconda2
 
 start=`date +%s`
 
@@ -24,14 +25,10 @@ echo "CPU: $CPU"
 echo "N: $N"
 
 echo "Simulate random mPings in genome without using frequency matrix (only use the matrix to determine strad)"
-#python mPing_sim_9merTSDV2_Random.py --input pictogram/somatic.tsd.matrix --output simulateV2_Random_TSD9mer_somaticMat --replicate 1 --size 1000 --use_freq 0
-#python mPing_sim_9merTSDV2_Random.py --input pictogram/somatic.tsd.matrix --output simulateV2_Random_TSD9mer_somaticMat --replicate 2 --size 1000 --use_freq 0
-#python mPing_sim_9merTSDV2_Random.py --input pictogram/somatic.tsd.matrix --output simulateV2_Random_TSD9mer_somaticMat --replicate 3 --size 1000 --use_freq 0
+python mPing_sim_9merTSDV2_Random.py --input pictogram/somatic.tsd.matrix --output simulateV2_Random_TSD9mer_somaticMat --replicate $N --size 1000 --use_freq 0
 
 echo "Simulate random mPings in genome using frequency matrix (this will be slower than the above one)"
-#python mPing_sim_9merTSDV2_Random.py --input pictogram/somatic.tsd.matrix --output simulateV2_Random_TSD9mer_somaticMat --replicate 4 --size 1000 --use_freq 1
-python mPing_sim_9merTSDV2_Random.py --input pictogram/somatic.tsd.matrix --output simulateV2_Random_TSD9mer_somaticMat --replicate 5 --size 1000 --use_freq 1
-python mPing_sim_9merTSDV2_Random.py --input pictogram/somatic.tsd.matrix --output simulateV2_Random_TSD9mer_somaticMat --replicate 6 --size 1000 --use_freq 1
+python mPing_sim_9merTSDV2_Random.py --input pictogram/somatic.tsd.matrix --output simulateV2_Random_TSD9mer_somaticMat_wFreq --replicate $N --size 1000 --use_freq 1
 
 end=`date +%s`
 runtime=$((end-start))
