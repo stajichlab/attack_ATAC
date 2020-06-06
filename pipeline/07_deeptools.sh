e348c7ff05f9f4ac8ae905fa3cad675e538c6bba
Ying Sun <ysun@ucr.edu>
2020-06-06 00:31:23 -0700

    should work but so slow, why?

--- pipeline/07_deeptools.sh
+++ pipeline/07_deeptools.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
-#SBATCH -p short --mem 24gb -N 1 -n 24 --out logs/shared_sites.log
+#SBATCH -p short --mem 24gb -N 1 -n 24 --out logs/deeptools.log
 
 module load samtools
 module load deeptools/3.4.3
+module unload python/2.7.5
+module load miniconda3
+source activate deeptools
 
 CPU=$SLURM_CPUS_ON_NODE
 if [ -z $CPU ]; then
@@ -10,48 +13,41 @@ if [ -z $CPU ]; then
 fi
 
 
-if [ $# -lt  7]; then
-    echo "usage:sh 07_deeptools.sh <mping_sites_to_use>"
+if [ $# -lt  3]; then
+    echo "usage:sh 07_deeptools.sh <mping_sites_to_use> <peaks> <density-file> <tags> <chrom_info> <contig_prefix>"
     echo "
-    <output_folder_name>: this scripts will create an folder under current path;
-    <peaks>: the output filtered peaks from choose_cutoff.sh, it should include three column, <chr> <start> <end> and it's 0 based;
-    <density-file>: the ouput *.density file from the calculate_density step;
-    <tag>: the output tag folder from prepareTag;
-    <chrom_info>: a file include the chromosome name;
-    <contig_prefix>: the name of the contig prefix. If there're no contig, set it as <no>;
+    <Data_analyzed>: ATAC or H3K27me3 or H3K36me3 or H3K56ac
+    <output_folder_name>: 03_processed_empty or 04_background_sites or 05_processed_shared
+    <processedfolder>: processed_background or processed_empty or processed_shared
+
     all the path should be the full path."
     exit -1; 
 fi
 
 #### mping_sites_to_use ####
-tmpfoldername=$1
-
+datatype=$1
+tmpfoldername=$2
+processed=$3
 
 BAM=bam
 MPING=data/mPing
 TMP=tmp/$tmpfoldername
-
-# genome lengths file
-cut -f 1,2 $GENOME.fai > $GENOME.genome
-COV=coverage/processed_shared
+COV=coverage/$processed/deeptools
+# make folder if it doesn't exist 
 mkdir -p $COV $TMP
 
-
-if [ ! -f $GENOME.fai ]; then
-	module load samtools
-	samtools faidx $GENOME
-fi
 
 for size in 250 500
 do
 	total=$(expr $size \* 2)
 	for strain in HEG4 EG4 A123 A119
 	do
-		BEDFILE=$TMP/${strain}_shared.${total}_window.bed
+		BEDFILE=$TMP/$tmpfoldername
 		NEWBASE=${strain}_shared.${total}
-        bamCoverage -b ${strain}.bam -o ${strain}.bw -bs=1 -p=max
-        computeMatrix reference-point -S ${strain}.bw -R ${strain}_shared.${total}_window.bed --upstream 1000 --downstream 1000 -out ${strain}${total}_computeMatrix --sortUsing max --skipZeros -p=max --maxThreshold 100000
-        plotHeatmap -m ${strain}${total}_computeMatrix -out ${strain}${total}_heatmap --colorList white,blue --sortUsing max
-        plotProfile -m ${strain}${total}_computeMatrix -out ${strain}${total}_profile --perGroup --plotType=fill
+        # I think this currently runs but it's ridiculously slow for some reason
+        bamCoverage -b $BAM/$datatype/${strain}.bam -o $COV/${strain}.bw -bs=1 -p=max
+        computeMatrix reference-point -S $COV/${strain}.bw -R $BEDFILE --upstream 2000 --downstream 2000 -out $COV/CMref${strain}${total} --binSize 50 --sortUsing max --skipZeros -p=max --maxThreshold 100000
+        plotHeatmap -m $COV/CMref${strain}${total} -out $COV/CMref${strain}${total}heatmap --colorList white,blue --sortUsing max
+        plotProfile -m $COV/CMref${strain}${total} -out $COV/CMref${strain}${total}profile --perGroup --plotType=fill
 	done
 done
