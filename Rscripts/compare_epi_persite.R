26dc066e5507f290b61dfa57540be3cd781aa1fa
Jason Stajich <jasonstajich.phd@gmail.com>
2020-06-06 13:42:10 -0700

    add framework for per-site epi compare sim and obs

---
+++ Rscripts/compare_epi_persite.R
@@ -0,0 +1,10 @@
+library(gridExtra)
+library(ggpubr)
+library(ggplot2)
+library(tidyr)
+library(tidyverse)
+library(pheatmap)
+library(RColorBrewer)
+statdir="coverage/processed_empty"
+bgdir="coverage/processed_background"
+
