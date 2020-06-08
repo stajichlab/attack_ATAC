1beaed2a3551cd6c6a9ddaaed3891ab1b37ac3c4
Ying Sun <ysun@ucr.edu>
2020-06-08 14:14:30 -0700

    run all together

--- pipeline/07_deeptools.sh
+++ pipeline/07_deeptools.sh
@@ -1,53 +1,17 @@
 #!/usr/bin/bash
-#SBATCH -p short --mem 24gb -N 1 -n 24 --out logs/deeptools.log
+#SBATCH -p short -N 1 -n 2 --mem 16gb --out logs/deeptools.log
 
-module load samtools
-module load deeptools/3.4.3
-module unload python/2.7.5
-module load miniconda3
-source activate deeptools
+sbatch ./pipeline/deeptools/07_deeptools_ATAC_shared.sh ATAC 05_processed_shared processed_shared
+sbatch ./pipeline/deeptools/07_deeptools_ChIP_shared.sh H3K27me3 05_processed_shared processed_shared
+sbatch ./pipeline/deeptools/07_deeptools_ChIP_shared.sh H3K36me3 05_processed_shared processed_shared
+sbatch ./pipeline/deeptools/07_deeptools_ChIP_shared.sh H3K56ac 05_processed_shared processed_shared
 
-CPU=$SLURM_CPUS_ON_NODE
-if [ -z $CPU ]; then
-	CPU=1
-fi
+sbatch ./pipeline/deeptools/07_deeptools_ATAC_empty.sh ATAC 03_processed_empty processed_empty
+sbatch ./pipeline/deeptools/07_deeptools_ChIP_empty.sh H3K27me3 03_processed_empty processed_empty
+sbatch ./pipeline/deeptools/07_deeptools_ChIP_empty.sh H3K36me3 03_processed_empty processed_empty
+sbatch ./pipeline/deeptools/07_deeptools_ChIP_empty.sh H3K56ac 03_processed_empty processed_empty
 
-
-if [ $# -lt  3]; then
-    echo "usage:sh 07_deeptools.sh <mping_sites_to_use> <peaks> <density-file> <tags> <chrom_info> <contig_prefix>"
-    echo "
-    <Data_analyzed>: ATAC or H3K27me3 or H3K36me3 or H3K56ac
-    <output_folder_name>: 03_processed_empty or 04_background_sites or 05_processed_shared
-    <processedfolder>: processed_background or processed_empty or processed_shared
-
-    all the path should be the full path."
-    exit -1; 
-fi
-
-#### mping_sites_to_use ####
-datatype=$1
-tmpfoldername=$2
-processed=$3
-
-BAM=bam
-MPING=data/mPing
-TMP=tmp/$tmpfoldername
-COV=coverage/$processed/deeptools
-# make folder if it doesn't exist 
-mkdir -p $COV $TMP
-
-
-for size in 250 500
-do
-	total=$(expr $size \* 2)
-	for strain in HEG4 EG4 A123 A119
-	do
-		BEDFILE=$TMP/$tmpfoldername
-		NEWBASE=${strain}_shared.${total}
-        # I think this currently runs but it's ridiculously slow for some reason
-        bamCoverage -b $BAM/$datatype/${strain}.bam -o $COV/${strain}.bw -bs=1 -p=max
-        computeMatrix reference-point -S $COV/${strain}.bw -R $BEDFILE --upstream 2000 --downstream 2000 -out $COV/CMref${strain}${total} --binSize 50 --sortUsing max --skipZeros -p=max --maxThreshold 100000
-        plotHeatmap -m $COV/CMref${strain}${total} -out $COV/CMref${strain}${total}heatmap --colorList white,blue --sortUsing max
-        plotProfile -m $COV/CMref${strain}${total} -out $COV/CMref${strain}${total}profile --perGroup --plotType=fill
-	done
-done
+sbatch ./pipeline/deeptools/07_deeptools_ATAC_background.sh ATAC 04_background_sites processed_background
+sbatch ./pipeline/deeptools/07_deeptools_ChIP_background.sh H3K27me3 04_background_sites processed_background
+sbatch ./pipeline/deeptools/07_deeptools_ChIP_background.sh H3K36me3 04_background_sites processed_background
+sbatch ./pipeline/deeptools/07_deeptools_ChIP_background.sh H3K56ac 04_background_sites processed_background
