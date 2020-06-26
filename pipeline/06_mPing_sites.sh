9dc9443781238c22dcba7e71bb225bec2ae3b04a
Ying Sun <ysun@ucr.edu>
2020-06-26 10:46:39 -0700

    updated site scripts

--- pipeline/06_mPing_sites.sh
+++ pipeline/06_mPing_sites.sh
@@ -38,12 +38,13 @@ bedtools intersect -a $MPING/EG4.gff -b $MPING/EG4.gff -wa | cut -f 1,4,5,9 | pe
 bedtools intersect -a $MPING/HEG4.gff -b $MPING/HEG4.gff -wa | cut -f 1,4,5,9 | perl -p -e 's/^Chr//; s/ID=//' > $TMP/HEG4_mPing_sites.bed
 bedtools intersect -a $MPING/A119.gff -b $MPING/A119.gff -wa | cut -f 1,4,5,9 | perl -p -e 's/^Chr//; s/ID=//' > $TMP/A119_mPing_sites.bed
 bedtools intersect -a $MPING/A123.gff -b $MPING/A123.gff -wa | cut -f 1,4,5,9 | perl -p -e 's/^Chr//; s/ID=//' > $TMP/A123_mPing_sites.bed
+bedtools intersect -a $MPING/NP.gff -b $MPING/NP.gff -wa | cut -f 1,4,5,9 | perl -p -e 's/^Chr//; s/ID=//' > $TMP/NP_mPing_sites.bed
 
 # use -b to add to both sides
 for size in 250 500
 do
 	total=$(expr $size \* 2)
-	for strain in HEG4 EG4 A123 A119
+	for strain in HEG4 EG4 A123 A119 NP
 	do
 		BEDFILE=$TMP/${strain}_mPing.${total}_window.bed
 		NEWBASE=${strain}_mPing.${total}
